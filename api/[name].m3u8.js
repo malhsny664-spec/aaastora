e01d628885aca5850a919ee2c234a32241c85cda
malhsny664-spec <malhsny664@gmail.com>
2026-04-22 15:10:14 +0200

    Update [name].m3u8.js

--- api/[name].m3u8.js
+++ api/[name].m3u8.js
@@ -5,13 +5,25 @@ export default async function handler(req, res) {
     const { name } = req.query;
 
     const url = channels[name];
+
+    if (!url) {
+      return res.status(404).send("Channel not found");
+    }
+
     const response = await fetch(url);
     let text = await response.text();
+
+    const host = "https://on-tv.site";
 
     text = text.split("\n").map(line => {
 
       if (line.startsWith("#")) return line;
       if (!line.trim()) return line;
+
+      // تحويل الرابط النسبي لكامل
+      if (line.startsWith("/")) {
+        line = host + line;
+      }
 
       // استخراج الرابط المشفر
       const match = line.match(/url=(.*)/);
@@ -19,7 +31,6 @@ export default async function handler(req, res) {
       if (match) {
         const encoded = match[1];
 
-        // 👇 ده اللي هيظهر للمستخدم
         return `https://aaastora.vercel.app/api/ts?url=${encoded}`;
       }
 
@@ -28,6 +39,9 @@ export default async function handler(req, res) {
     }).join("\n");
 
     res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Cache-Control", "no-store");
+
     return res.status(200).send(text);
 
   } catch (e) {
