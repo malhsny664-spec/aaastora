725ddd7be8114543c01365995eb2c7b15c4e1ff0
malhsny664-spec <malhsny664@gmail.com>
2026-04-22 14:39:30 +0200

    Update and rename [channel].js to bein.js

--- api/bein.js
+++ api/bein.js
@@ -2,32 +2,23 @@ import channels from "../channels.json";
 
 export default async function handler(req, res) {
   try {
-    const { channel } = req.query;
+    const { name } = req.query;
 
-    const url = channels[channel];
+    const url = channels[name];
 
-    if (!url) return res.status(404).send("Not found");
+    if (!url) return res.status(404).send("Channel not found");
 
     const response = await fetch(url);
     let text = await response.text();
 
-    const base = "https://aaastora.vercel.app";
-
-    let index = 27970;
+    let index = 20000;
 
     text = text.split("\n").map(line => {
-
-      if (line.startsWith("#")) {
-        return line;
-      }
-
+      if (line.startsWith("#")) return line;
       if (!line.trim()) return line;
 
       index++;
-
-      // تحويل أي segment إلى TS وهمي
-      return `media_${channel}_${index}.ts`;
-
+      return `https://aaastora.vercel.app/ts?file=${index}.ts`;
     }).join("\n");
 
     res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
