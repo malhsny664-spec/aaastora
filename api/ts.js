d63e49555cc2d8f2f88668fa3b3e336e6aa0f886
malhsny664-spec <malhsny664@gmail.com>
2026-04-22 14:46:24 +0200

    Update ts.js

--- api/ts.js
+++ api/ts.js
@@ -1,22 +1,20 @@
 export default async function handler(req, res) {
   try {
-    const { file } = req.query;
-
-    if (!file) return res.status(400).send("missing file");
-
-    // نفس المصدر الحقيقي
     const url =
-      "https://on-tv.site/Blackcode/index.php?action=stream&id=4128&cat=333";
+      "https://on-tv.site/Blackcode/?action=stream&id=4128&cat=333";
 
     const response = await fetch(url);
-    const buffer = await response.arrayBuffer();
+
+    if (!response.ok) {
+      return res.status(500).send("stream error");
+    }
 
     res.setHeader("Content-Type", "video/mp2t");
     res.setHeader("Access-Control-Allow-Origin", "*");
 
-    return res.status(200).send(Buffer.from(buffer));
+    response.body.pipe(res);
 
   } catch (e) {
-    return res.status(500).send("error");
+    res.status(500).send(e.message);
   }
 }
